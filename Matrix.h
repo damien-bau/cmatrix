844df26e5c5edd1f4153daf1fb9d36f86b6480e8
Damien Bau <baudamien@gmail.com>
2025-12-15 18:22:54 +0100

    rough outline

--- Matrix.h
+++ Matrix.h
@@ -7,11 +7,30 @@ class Matrix {
         Matrix(int, int);
 
         // Operator overloads
-        Matrix& operator +=(const Matrix&);
-        Matrix& operator -=(const Matrix&);
-        Matrix& operator *=(const Matrix&);
-        Matrix& operator /=(const Matrix&);
-        Matrix& operator ^(const Matrix&);
+        Matrix& operator = (const Matrix&);
+        friend Matrix operator + (const Matrix&, const Matrix&);
+        friend Matrix operator - (const Matrix&, const Matrix&);
+        friend Matrix operator * (const Matrix&, const Matrix&);
+        friend Matrix operator * (double, const Matrix&);
+        friend Matrix operator * (const Matrix&, double);
+        friend Matrix operator / (const Matrix&, double);
+        Matrix& operator += (const Matrix&);
+        Matrix& operator -= (const Matrix&);
+        Matrix& operator *= (const Matrix&);
+        Matrix& operator *= (double);
+        Matrix& operator /= (double);
+        Matrix& operator ^ (int);
+
+
+        // Member functions
+        Matrix transpose();
+        Matrix inverse();
+        Matrix identity(int);
+        Matrix dotproduct(Matrix, Matrix);
+        Matrix crossproduct(Matrix, Matrix);
+
+    private:
+        int rows, cols;
 
 
 };
